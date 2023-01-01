2b6241d5d02f9e590f46fb4021f297303c22d718
Artem Khusnullin <twentyfortythree@yandex.ru>
2023-01-01 22:46:34 +0500

    Внёс правки по требрванию ревьюера

--- script/index.js
+++ script/index.js
@@ -1,27 +1,29 @@
 let editButton = document.querySelector('.profile__button_type_edit');
 let popupCloseButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let popupNameInput = document.querySelector('#name');
-popupNameInput.value = profileName.textContent;
 let popupDescriptionInput = document.querySelector('#description');
-popupDescriptionInput.value = profileDescription.textContent;
+let formElement = document.querySelector('.popup__form');
 
-
-function popup() {
-  let popup = document.querySelector('.popup');
-  popup.classList.toggle('popup_opened');
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  popupNameInput.value = profileName.textContent;
+  popupDescriptionInput.value = profileDescription.textContent;
 }
 
-let formElement = document.querySelector('.popup__form');
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let name = popupNameInput.value;
-  let description = popupDescriptionInput.value;
-  profileName.textContent = name;
-  profileDescription.textContent = description;
-  popup();
+  profileName.textContent = popupNameInput.value;
+  profileDescription.textContent = popupDescriptionInput.value;
+  popupClose();
 }
+
 formElement.addEventListener('submit', handleFormSubmit);
-editButton.addEventListener('click', popup);
-popupCloseButton.addEventListener('click', popup);
+editButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
